59cc28e298e8e49cb87d7840e5ebf7705c559c5e
Morsul <81906186+Morsul@users.noreply.github.com>
2023-02-14 23:35:29 +0200

    feat: add tracking of requests

--- src/controler/scoreController.ts
+++ src/controler/scoreController.ts
@@ -11,14 +11,23 @@ export class ScoreHandler{
   }
 
   addScore = (req:Request, res:Response) => {
+    console.log('start addScore')
     const score = req.body;
     
     this._collection.insertOne(score)
-    .then((result: InsertOneResult<IScore>)=>res.status(responseStatus.created).json(result))
-    .catch((err: any)=>res.status(responseStatus.error).json({err: 'Could not save score'}))
+    .then((result: InsertOneResult<IScore>)=>{
+      console.log('end addScore')
+      res.status(responseStatus.created).json(result)
+    })
+    .catch((err: any)=>{
+      console.log('Error addScore')
+      res.status(responseStatus.error).json({err: 'Could not save score'})
+    })
+    
   };
 
   getScore =(req:Request, res:Response)=>{
+    console.log("start getScore")
     const {query} = req;
     const searchQuery = query.level === undefined ? {} : {level: Number(query.level)}
     let limit = 0
@@ -40,7 +49,14 @@ export class ScoreHandler{
     .sort(    
       {[query.sort as string]: (sortQuery.order as any)[query.order as string]}
     ).toArray()
-    .then((result: WithId<IScore>[])=> res.status(responseStatus.ok).json(result))
-    .catch((err: any)=>res.status(responseStatus.error).json({err: 'Could not get score'}))
+    .then((result: WithId<IScore>[])=> {
+        console.log("end getScore");
+        res.status(responseStatus.ok).json(result
+    )})
+    .catch((err: any)=>{
+      console.log('Error addScore')
+      res.status(responseStatus.error).json({err: 'Could not get score'})
+    })
+    
   }
 }
